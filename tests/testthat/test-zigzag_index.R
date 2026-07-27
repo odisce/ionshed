b9eecf7918b8e13f6397ddd82d82524e9f6b7b32
sdechaumet <sylvain.dechaumet@cea.fr>
2026-07-27 10:38:53 +0200

    up test

--- tests/testthat/test-zigzag_index.R
+++ tests/testthat/test-zigzag_index.R
@@ -1,9 +1,9 @@
 testthat::test_that("zigzag_index()", {
   norm_dist <- c(1, 1, 2, 3, 5, 10, 5, 8, 2, 1, 1) |> zigzag_index()
   rand_dist <- c(1, 6, 5, 3, 9, 5, 6, 1, 8, 3, 5, 8) |> zigzag_index()
-  testthat::expect_true(
-    rand_dist[is.finite(rand_dist)] > norm_dist[is.finite(norm_dist)]
-  )
+  message(c("rand_dist: ", rand_dist))
+  message(c("norm_dist: ", norm_dist))
+  testthat::expect_true(rand_dist > norm_dist)
 })
 
 testthat::test_that("zigzag_index() < 4", {
