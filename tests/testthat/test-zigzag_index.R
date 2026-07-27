a2505bead741eff65220cd02f4ee9a0ef1504ecc
sdechaumet <sylvain.dechaumet@cea.fr>
2026-07-27 11:01:39 +0200

    up test

--- tests/testthat/test-zigzag_index.R
+++ tests/testthat/test-zigzag_index.R
@@ -1,6 +1,6 @@
 testthat::test_that("zigzag_index()", {
-  norm_dist <- c(1, 1, 2, 3, 5, 10, 5, 8, 2, 1, 1) |> zigzag_index()
-  rand_dist <- c(1, 6, 5, 3, 9, 5, 6, 1, 8, 3, 5, 8) |> zigzag_index()
+  norm_dist <- c(1, 2, 3, 5, 3, 2, 1) |> zigzag_index()
+  rand_dist <- c(1, 6, 5, 3, 9, 5, 4) |> zigzag_index()
   message(c("rand_dist: ", rand_dist))
   message(c("norm_dist: ", norm_dist))
   testthat::expect_true(rand_dist > norm_dist)
